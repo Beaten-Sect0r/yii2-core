9325d30df4b159400c69d3093e667dbd3e02f594
Alexander Volkov <the.purgen@gmail.com>
2017-07-09 09:33:50 +0300

    fix for dotenv boolean

--- common/shortcuts.php
+++ common/shortcuts.php
@@ -7,20 +7,16 @@
  * @param  mixed $default
  * @return mixed
  */
-function env($key, $default = false)
+function env($key, $default = null)
 {
     $value = getenv($key);
-
     if ($value === false) {
         return $default;
     }
-
     switch (strtolower($value)) {
         case 'true':
-        case '(true)':
             return true;
         case 'false':
-        case '(false)':
             return false;
     }
 
